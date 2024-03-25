d02e07b1ddaf0216d5a8f680d9f4cc86d694f367
Pedro Luca Prates <pedrolucaofc@outlook.com>
2024-03-25 00:10:35 -0300

    fix: consertando path da página 404

--- src/pages/Aluno/Aluno.jsx
+++ src/pages/Aluno/Aluno.jsx
@@ -21,19 +21,19 @@ export function Aluno() {
       <p>Escola: Escola X</p>
       <h2>Sua olimpíada:</h2>
       <div className="olimp-container">
-        {
+        {/* {
           olimpiadas.map(function(val) {
             return (
               <div className="olimpiada">
                 <h3>Olimpíada de {val.titulo}</h3>
-                {/* <p>Data: {val.data}</p>
-                <p>Hora: {val.hora}</p> */}
+                <p>Data: {val.data}</p>
+                <p>Hora: {val.hora}</p>
                 <p>Pontuação: {val.conteudo}</p>
                 <BtnAcessar />
               </div>
             )
           })
-        }
+        } */}
         <div className="olimpiada">
           <h3>Olimpíada de Química</h3>
           <p>Data: 21/04/2024</p>
