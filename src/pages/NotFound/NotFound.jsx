4782c93e936ba15bca720201837afd2853b49bde
Pedro Luca Prates <pedrolucaofc@outlook.com>
2024-04-25 12:39:26 -0300

    fix: corrigir posicionament de textos h1 no centro

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import './styles.css'
 export function NotFound() {
   return (
     <div className="container-not-found under-header-container">
-      <h1>404 - Página não encontrada</h1>
+      <h1 className='notfound-h1'>404 - Página não encontrada</h1>
       <img src={Img4042} alt="erro 404" />
       <p>A página que você tentou acessar não existe, por favor verifique se o link está correto</p>
     </div>
