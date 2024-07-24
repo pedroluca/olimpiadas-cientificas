9d2243216cb34a116d77af8a48945d2d1c6a7c5e
pedroluca <pedrolucaofc@outlook.com>
2024-07-24 14:59:36 -0300

    feat: add team members modal to present and disclose who developed the web system

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import LogoIF from '../../assets/images/if-bw.png'
 import QRCode from '../../assets/images/qrcode.svg'
 import Facebook from '../../assets/images/facebook-color.svg'
 import Youtube from '../../assets/images/youtube-color.svg'
 import Instagram from '../../assets/images/instagram-color.svg'
 import './styles.css'
+import { TeamModal } from '../TeamModal/team-modal'
 
 export function Footer() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
   return (
     <footer className="footer">
       <section className="section1">
@@ -19,12 +31,17 @@ export function Footer() {
       <section className="section2">
         <h2>I OLIMPÍADAS CIENTÍFICAS DO SERTÃO PRODUTIVO</h2>
         <p className='paragrafo-central'>As Olimpíadas Científicas buscam mobilizar estudantes dos municípios que compõem o Território Sertão Produtivo, valorizando a criatividade, a atitude científica e a inovação, com objetivo de fomentar a pesquisa e a experiência tecnológica entre os jovens.</p>
-        <p className='copyright'>Desenvolvido com &hearts; por estudantes do curso de ADS do IF Baiano - <em>Campus</em> Guanambi</p>
+        <p className='copyright' onClick={openModal}>
+          Desenvolvido com &hearts; por estudantes do curso de ADS do IF Baiano - <em>Campus</em> Guanambi
+        </p>
       </section>
       <section className="section3">
         <h3>INSCREVA-SE</h3>
         <img src={QRCode} className='qrcode' />
       </section>
+      {isModalOpen && (
+        <TeamModal onClick={closeModal} />
+      )}
     </footer>
-  )
+  );
 }
---
+++ src/components/TeamModal/team-modal.jsx
@@ -0,0 +1,53 @@
+import './styles.css'
+import PropTypes from 'prop-types'
+import DevPedro from '../../assets/images/dev-pedro.png'
+import DevAnthonius from '../../assets/images/dev-anthonius.jpg'
+import DevTharlis from '../../assets/images/dev-tharlis.jpg'
+import DevSavio from '../../assets/images/dev-savio.jpeg'
+
+export function TeamModal({ onClick }) {
+
+  const handleModalClick = (event) => {
+    if (event.target === event.currentTarget) {
+      onClick()
+    }
+  }
+
+  const handleMemberClick = (url) => {
+    window.location.href = url
+  }
+
+  return (
+    <div className='modal-container' onClick={handleModalClick}>
+      <div className='modal-content'>
+        <h2>Desenvolvido por</h2>
+        <div className="team-holder">
+          <div className="team-member" onClick={() => handleMemberClick('https://pedroluca.vercel.app')}>
+            <img src={DevPedro} alt='Foto' />
+            <h3>Pedro Luca Prates</h3>
+            <p>Desenvolvedor Full Stack</p>
+          </div>
+          <div className="team-member" onClick={() => handleMemberClick('https://github.com/anthoniusdev')}>
+            <img src={DevAnthonius} alt='Foto' />
+            <h3>Anthonius Figueiredo</h3>
+            <p>Desenvolvedor</p>
+          </div>
+          <div className="team-member" onClick={() => handleMemberClick('https://www.linkedin.com/in/tharlis-fábio/')}>
+            <img src={DevTharlis} alt='Foto' />
+            <h3>Tharlis Fábio</h3>
+            <p>Desenvolvedor Full Stack</p>
+          </div>
+          <div className="team-member" onClick={() => handleMemberClick('https://github.com/saviodev23')}>
+            <img src={DevSavio} alt='Foto' />
+            <h3>Sávio Santos</h3>
+            <p>Desenvolvedor Backend</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+TeamModal.propTypes = {
+  onClick: PropTypes.func,
+}
